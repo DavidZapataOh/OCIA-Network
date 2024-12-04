bf5333ab5ce07208a92eb0b16427159c8b7e4a74
0xtdvt <davidzapata5885@gmail.com>
2024-12-04 15:14:19 -0500

    api fix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,25 @@
 /** @type {import('next').NextConfig} */
+const dotenv = require("dotenv");
+dotenv.config();
+
 const nextConfig = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(js|d.ts)\.map$/,
-        use: ['ignore-loader'],
-      });
-      
-      config.module.rules.push({
-        test: /\.d\.ts$/,
-        use: ['ignore-loader'],
-      });
-      
-      return config;
-    },
-  }
-  
-  module.exports = nextConfig
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(js|d.ts)\.map$/,
+      use: ["ignore-loader"],
+    });
+
+    config.module.rules.push({
+      test: /\.d\.ts$/,
+      use: ["ignore-loader"],
+    });
+
+    return config;
+  },
+  env: {
+    NEXT_PUBLIC_BRIAN_API_KEY: process.env.NEXT_PUBLIC_BRIAN_API_KEY,
+    NEXT_PUBLIC_BRIAN_API_URL: process.env.NEXT_PUBLIC_BRIAN_API_URL,
+  },
+};
+
+module.exports = nextConfig;
